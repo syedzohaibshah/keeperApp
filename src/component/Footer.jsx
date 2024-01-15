3d1fa16672f6f25c6704d614d5f8a25f91e1c88d
syedzohaibshah <zs3783999g@gmail.com>
2024-01-15 20:26:57 +0500

    initial commit

---
+++ src/component/Footer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const time = new Date().getFullYear();
+function Footer(){
+    return(
+        <footer>
+            <p>Copy Right {time}</p>
+        </footer>
+            
+            
+        
+    );
+}
+
+
+export default Footer;
